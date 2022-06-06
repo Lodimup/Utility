48ea53f7e9bfb1be6cc3dbfd43a90f7b2503a840
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2022-06-07 00:48:40 +0700

    ADD

---
+++ comma_separate_number.py
@@ -0,0 +1,16 @@
+"""
+Demonstrates how to separate string representation using ,
+every 3 digits of number using f string
+"""
+
+for i in range(8):
+    print(f'{64**i:,}')
+
+# 1
+# 64
+# 4,096
+# 262,144
+# 16,777,216
+# 1,073,741,824
+# 68,719,476,736
+# 4,398,046,511,104
